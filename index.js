c612b65d68e7b6d550435a04c4e28da085d7d199
GitStewie <48700222+GitStewie@users.noreply.github.com>
2020-10-10 21:05:04 -0600

    Update index.js
    Currently Rewriting Website

--- index.js
+++ index.js
@@ -6,24 +6,39 @@ const linkArray = [
     { "name": "My Favorite Place to Take Interesting Classes Outside School!", "url": "https://www.coursera.org/" },
  ]
 
-addEventListener('fetch', event => {
+ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
 /**
- * Respond with hello worker text
- * @param {Request} request
+ * LinkTransformer allows retrieved HTML to be rewritten easily
+ * @param {Async function} links // Modify HTML elements
  */
+class LinksTransformer {
+  constructor(links) {
+    this.links = links
+  }
+  async element(element) {
+    this.element(element)
+  }
+}
+
+ /**
+   * Handles a FETCH request to the worker.
+   * @param  {Request} request Request Promise from the FETCH event to the worker.
+   * 
+   */
 async function handleRequest(request) {
   // Deploy a JSON API
   if (request.url.endsWith('/links')) {
     return new Response(JSON.stringify(linkArray), { headers: { "content-type": "application/json;charset=UTF-8" } })
   }
   /* Test Functionality of JSON by visiting http://127.0.0.1:8787/links */
-  
-  // Retrieve static HTML page
-  const template = await fetch('https://static-links-page.signalnerve.workers.dev/static/html', { headers: { "content-type": "text/html;charset=UTF-8" } })
 
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  // Retrieve static HTML page
+  const retrievedHTML = await fetch('https://static-links-page.signalnerve.workers.dev/static/html', { headers: { "content-type": "text/html;charset=UTF-8" } })
+  rewriter = new HTMLRewriter();
+  return new HTMLRewriter()
+    .on("div#profile", new LinksTransformer(async(element) => element.removeAttribute('style')))
+    .transform(retrievedHTML);
 }
