7682755e406be85adf6cec50f8fa0ab0f710510e
GitStewie <48700222+GitStewie@users.noreply.github.com>
2020-10-10 19:29:26 -0600

    Update index.js
    Set-up Array of Buttons!

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Array of Links, Corresponds to Buttons on Website
+const links = [ 
+    { "name": "Feeding America - The Charity I Walk For!", "url": "https://www.feedingamerica.org/" },
+    { "name": "My Dog Stewie's Breed!", "url": "https://dogtime.com/dog-breeds/maltipoo#/slide/1" },
+    { "name": "Gravity Net", "url": "https://mrdoob.com/projects/chromeexperiments/google-gravity/" },
+    { "name": "My Favorite Place to Take Interesting Classes Outside School!", "url": "https://www.coursera.org/" },
+ ]
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
